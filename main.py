e9720719caa54f6ec0b272c721e8ac28dac928ba
ca-montenegro <ca.montenegro@uniandes.edu.co>
2018-11-16 18:41:57 -0500

    windowsOS version

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def get_platform():
 
 def dex2Jar(apk):
     plt = get_platform()
+    file = apk.split('.')
     if(plt=='OS X' or plt=='Linux'):
-    	file = apk.split('.')
     	print('--Setup d2j_invoke.sh permissions--')
     	subprocess.call(['chmod', '+x', './dex2jar-2.0/d2j_invoke.sh'])
     	print('--APK to dex--')
@@ -41,16 +41,19 @@ def dex2Jar(apk):
     	subprocess.call(['java','-jar','jd-core-java-1.2.jar',jarFile])
     	print('--Android app java files are now available in the project folder '+ file[0] + '.src')
     elif(get_platform()=='Windows'):
-        print('--APK to Jar initiated--')
-        subprocess.call(['dex2jar-2.0'+chr(92)+'d2j-dex2jar.bat', apk])
-        print('--Jar created. Creating .class Java files')
-        file = apk.split('.')
-        createFolder('./' + file[0])
-        jarFile = file[0] + '-dex2jar.jar'
-        subprocess.call(['java','-jar','jd-core-java-1.2.jar',jarFile])
-        print('--Android app java files are now available in the project folder '+ file[0] + '.src')
-
-
+    	print('--APK to dex--')
+    	subprocess.call(["dex2jar-2.0"+chr(92)+"d2j-jar2dex.bat", apk])
+    	subprocess.call(['rename',file[0]+'-jar2dex.dex',file[0]+'.dex'])
+    	print('--Dex files generated in',file[0]+'.dex--')
+    	print('--Dex to Smali initiated--')
+    	subprocess.call(['dex2jar-2.0'+chr(92)+'d2j-dex2smali.bat',file[0]+'.dex'])
+    	subprocess.call(['rename',file[0]+'-out',file[0]+'-smali'])
+    	print('--Converting to JAR files--')
+    	subprocess.call(['dex2jar-2.0'+chr(92)+'d2j-dex2jar.sh',file[0]+'.dex'])
+    	print('--Jar created as ',file[0]+'-dex2jar.jar'+'. Creating .java files')
+    	jarFile = file[0] + '-dex2jar.jar'
+    	subprocess.call(['java','-jar','jd-core-java-1.2.jar',jarFile])
+    	print('--Android app java files are now available in the project folder '+ file[0] + '.src')
 
 # --setup--
 def main(argv):
